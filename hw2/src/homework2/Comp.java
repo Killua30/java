e1e3fdf5b1be3e7c624805371a9c91c87ec56a1b
Killua30 <71076034+Killua30@users.noreply.github.com>
2020-10-01 10:45:46 +0300

    Переделал дз2

---
+++ hw2/src/homework2/Comp.java
@@ -0,0 +1,8 @@
+package homework2;
+
+public abstract interface Comp {
+
+    String turnOn();
+    String turnOff();
+    String connect();
+}
---
+++ hw2/src/homework2/Desktop.java
@@ -0,0 +1,9 @@
+package homework2;
+
+public class Desktop extends Computer {
+
+    public Desktop(String maker, String model, String proc, String ram, String disk) {
+        super(maker, model, proc, ram, disk);
+    }
+
+}
